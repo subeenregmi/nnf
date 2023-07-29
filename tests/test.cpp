4be0b14b597b43ed9f39046663a18bf098a57c0b
subeenregmi <subeenregmi123@gmail.com>
2023-07-29 01:04:23 +0100

    added indexing for matricies

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,10 @@
 #include "../include/matrix.hpp"
 
 int main(){
+	NMatrix::Matrix mat = {.rows=2, .cols=2};
+	NMatrix::MEMORY_ALLOC(&mat);
+	NMatrix::PRINT(&mat);
+	mat[1][1] = 10.0f;
+	NMatrix::PRINT(&mat);
 	return 0;
 }
